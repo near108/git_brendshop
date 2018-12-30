8a95ee29ca9620e8c7d8ced046a78a7989682ae3
EC2 Default User <ec2-user@ip-172-31-20-122.ap-southeast-1.compute.internal>
2018-12-30 04:34:07 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-User.create(email: 'masayoshi108justin@gmail.com', password: 'momotora108')
+User.create(email: 'masayoshi108justin@gmail.com', password: 'momotora108', status: '1')
 Bean.create(country: 'ブラジル', grade: 'サントスNo.2', loast: 'シティーロースト', comment: "初期値として設定")
