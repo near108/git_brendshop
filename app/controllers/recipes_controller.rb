3527db2a0556800d7c099bda2a4bf73f9c34b5c7
EC2 Default User <ec2-user@ip-172-31-20-122.ap-southeast-1.compute.internal>
2018-12-30 14:01:31 +0000

    レシピ新規作成画面の作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,22 @@
 class RecipesController < ApplicationController
   
   def index
+    @recipe = Recipe.all
     @user = User.find(current_user.id)
   end
   
   def new
     @recipe = Recipe.all
     @bean = Bean.all
+  end
+  
+  def create
+    Recipe.create(recipe_params)
+  end
+
+private
+  def recipe_params
+    params.permit(:recipe_name, :bean_1_id, :amount_1, :bean_2_id, :amount_2, :bean_3_id, :amount_3).merge(user_id: current_user.id)
   end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   
+  belongs_to :bean_1, foreign_key: :bean_1_id, class_name: "Bean"
+  belongs_to :bean_2, foreign_key: :bean_2_id, class_name: "Bean"
+  belongs_to :bean_3, foreign_key: :bean_3_id, class_name: "Bean"
+  
   belongs_to :user
 end
