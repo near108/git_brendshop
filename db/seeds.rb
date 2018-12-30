79ed1f354f4ac4b268b502aa1dd01022479efeb0
EC2 Default User <ec2-user@ip-172-31-20-122.ap-southeast-1.compute.internal>
2018-12-30 06:24:06 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 require "csv"
 beans_csv = CSV.readlines("db/beans.csv")
 beans_csv.each do |row|
-  Bean.create(country: row[1], grade: row[2], roast: row[3], comment: [4])
+  Bean.create(id: [0], country: row[1], grade: row[2], roast: row[3], comment: [4])
 end
